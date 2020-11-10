cc89f0188d1b729befdd40d6dc730dcff44aba8c
Jack Proudfoot <jsp50@duke.edu>
2020-11-10 18:23:26 -0500

    Added serial line

--- FPGA/uart_echo.v
+++ FPGA/uart_echo.v
@@ -1,14 +1,15 @@
 `timescale 1ns/10ps
  
-module uart_echo(fpga_clock);
+module uart_echo(fpga_clock, serial_line);
 
     input fpga_clock;
+    input serial_line;
 
-    wire clock;
+    reg clock;
 
     // create 10Mhz clock from 100 MHz
     integer clock_cnt;
-    always @(posedge clock) begin
+    always @(posedge fpga_clock) begin
         if (clock_cnt == 9) begin
             clock <= ~clock;
             clock_cnt <= 0;
@@ -32,7 +33,7 @@ module uart_echo(fpga_clock);
 
     uart_rx #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) UART_RX_INST
     (.i_Clock(clock),
-        .i_Rx_Serial(r_Rx_Serial),
+        .i_Rx_Serial(serial_line),
         .o_Rx_DV(),
         .o_Rx_Byte(w_Rx_Byte)
         );
@@ -42,13 +43,13 @@ module uart_echo(fpga_clock);
         .i_Tx_DV(r_Tx_DV),
         .i_Tx_Byte(r_Tx_Byte),
         .o_Tx_Active(),
-        .o_Tx_Serial(),
+        .o_Tx_Serial(serial_line),
         .o_Tx_Done(w_Tx_Done)
         );
 
 
     integer cooldown = 0;
-    byte count = 0;
+    integer count = 0;
 
     always @(posedge clock) begin
         if (cooldown == 1000) begin
@@ -56,7 +57,7 @@ module uart_echo(fpga_clock);
             count <= count + 1;
 
             r_Tx_DV <= 1'b1;
-            r_Tx_Byte <= count;
+            r_Tx_Byte <= 8'ha7;
         end else begin
             cooldown <= cooldown + 1;
             r_Tx_DV <= 1'b0;
