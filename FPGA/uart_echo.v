0763ecdf0abd8ad402cce7d5c85c33a195033652
Jack Proudfoot <jsp50@duke.edu>
2020-11-10 19:10:32 -0500

    Replaced uart core

--- FPGA/uart_echo.v
+++ FPGA/uart_echo.v
@@ -1,9 +1,9 @@
 `timescale 1ns/10ps
  
-module uart_echo(fpga_clock, serial_line);
+module uart_echo(fpga_clock, reset, txd, rxd);
 
     input fpga_clock;
-    input serial_line;
+    input txd, rxd;
 
     reg clock;
 
@@ -25,27 +25,11 @@ module uart_echo(fpga_clock, serial_line);
     parameter c_CLKS_PER_BIT    = 87;
     parameter c_BIT_PERIOD      = 8600;
 
-    reg r_Tx_DV = 0;
-    wire w_Tx_Done;
-    reg [7:0] r_Tx_Byte = 0;
-    reg r_Rx_Serial = 1;
-    wire [7:0] w_Rx_Byte;
+    wire frmero, rxce, txmty, txce, bsy;
 
-    uart_rx #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) UART_RX_INST
-    (.i_Clock(clock),
-        .i_Rx_Serial(serial_line),
-        .o_Rx_DV(),
-        .o_Rx_Byte(w_Rx_Byte)
-        );
+    wire [7:0] tx, rx;
 
-    uart_tx #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) UART_TX_INST
-    (.i_Clock(clock),
-        .i_Tx_DV(r_Tx_DV),
-        .i_Tx_Byte(r_Tx_Byte),
-        .o_Tx_Active(),
-        .o_Tx_Serial(serial_line),
-        .o_Tx_Done(w_Tx_Done)
-        );
+    tiny_uart uart_core(reset, clock, txd, rxd, frmero, rx, rxce, tx, txmty, txce, bsy);
 
 
     integer cooldown = 0;
@@ -56,11 +40,11 @@ module uart_echo(fpga_clock, serial_line);
             cooldown <= 0;
             count <= count + 1;
 
-            r_Tx_DV <= 1'b1;
-            r_Tx_Byte <= 8'ha7;
+            txce <= 1'b1;
+            tx <= 8'ha7;
         end else begin
             cooldown <= cooldown + 1;
-            r_Tx_DV <= 1'b0;
+            txce <= 1'b0;
         end
     end
    
