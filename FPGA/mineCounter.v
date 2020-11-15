531a35a8eb0de6ccab461e1218f202559ae6b382
Jack Proudfoot <jsp50@duke.edu>
2020-11-15 00:26:18 -0500

    Modified sha to perform one slice per cycle

--- FPGA/mineCounter.v
+++ FPGA/mineCounter.v
@@ -1,11 +1,7 @@
-module mineCounter(q, clock, reset);
+module mineCounter #(parameter SIZE= 2) (q, clock, reset);
     input clock, reset;
 
-    output reg [1:0] q = 0;
-
-    // initial begin
-    //     q <= 2'b0;
-    // end
+    output reg [SIZE-1:0] q = 0;
 
     always @(posedge clock or posedge reset) begin
         if(~reset) begin
@@ -14,9 +10,4 @@ module mineCounter(q, clock, reset);
             q <= 0;
         end
     end
-
-    // always @(posedge clock) begin
-    //     q[0] <= (~q[1] & ~q[0] & clock) | (~q[1] & q[0] & ~clock & ~reset);
-    //     q[1] <= (~q[1] & ~q[0] & reset) | (~q[1] & q[0] & clock);
-    // end
 endmodule
